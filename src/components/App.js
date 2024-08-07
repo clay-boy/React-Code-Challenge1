5d6b6e2e94c4f29c4b4ec90b5c479cd58b6035bb
plug_keith <keith.mwai@student.moringaschool.com>
2024-08-07 13:01:22 +0300

    Fetched with appjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-
-import '../App.css';
-
+import { useEffect, useState } from "react";
+import "../App.css";
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((response) => response.json())
+      .then((transactions) => setTransactions(transactions))
+      // .console((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1 className="title">Bank-Transactions</h1>
+      <hr />
     </div>
   );
 }
-
 export default App;
