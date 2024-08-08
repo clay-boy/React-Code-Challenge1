688cb806310b2bd73dc23b979a72d7bedf4d17e8
plug_keith <keith.mwai@student.moringaschool.com>
2024-08-08 13:33:28 +0300

    changes

--- src/components/Addtransactions.js
+++ src/components/Addtransactions.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Addtransactions () = {
+const Addtransactions = ({ onsubmit }) => {
   const [date, setDate] = useState(""); 
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
